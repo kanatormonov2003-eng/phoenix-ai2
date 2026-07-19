ca89f92aa5f421daa6f6cb3d78375d6f5be5bfd7
kanatormonov2003-eng <kanatormonov2003@gmail.com>
2026-07-20 00:15:38 +0600

    Add files via upload

---
+++ backend/models_test.py
@@ -0,0 +1,16 @@
+from google import genai
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_KEY")
+)
+
+
+for model in client.models.list():
+
+    print(model.name)
